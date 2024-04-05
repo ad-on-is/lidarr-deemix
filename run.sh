53e76d7cf6f2b2f8e9789d8457cdadc3680b9f8b
Adis Durakovic <adis.durakovic@gmail.com>
2024-04-05 11:19:44 +0200

    fix: container depend

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ LC=$(printenv LIDARR_CONTAINER)
 nohup python ./python/deemix-server.py > ~/nohup_deemix.txt &
 nohup pnpm run start > ~/nohup_server.txt &
 docker exec -it $LC /bin/bash -c "update-ca-certificates" || true > /dev/null 2>&1
+docker restart $LC > /dev/null 2>&1
 mitmdump -s ./python/http-redirect-request.py
