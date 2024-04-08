8b40c7c4e5a56c134f34ddac3a8a87d586e5d3da
Adis Durakovic <adis.durakovic@gmail.com>
2024-04-08 15:27:33 +0200

    fix: pipe all output

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 LC=$(printenv LIDARR_CONTAINER)
-nohup python ./python/deemix-server.py > ~/nohup_deemix.txt &
-nohup pnpm run start > ~/nohup_server.txt &
+nohup python ./python/deemix-server.py > ~/nohup_deemix.txt 2>&1 &
+nohup pnpm run start > ~/nohup_server.txt 2>&1 &
 
-nohup mitmdump -s ./python/http-redirect-request.py > ~/nohup_mitmdump.txt &
+nohup mitmdump -s ./python/http-redirect-request.py > ~/nohup_mitmdump.txt 2>&1 &
 docker exec -it $LC /bin/bash -c "update-ca-certificates"
 tail -f ~/nohup_*.txt
