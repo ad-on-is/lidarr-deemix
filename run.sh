6f443bc5f80fe09950abc8a6c9cc07af6c0838d8
Adis Durakovic <adis.durakovic@gmail.com>
2024-04-09 12:22:29 +0200

    fix: update certs every hour

--- run.sh
+++ run.sh
@@ -6,4 +6,11 @@ nohup pnpm run start > ~/nohup_server.txt 2>&1 &
 
 nohup mitmdump -s ./python/http-redirect-request.py > ~/nohup_mitmdump.txt 2>&1 &
 docker exec -it $LC /bin/bash -c "update-ca-certificates"
+
+# update certs every hour
+while true ; do
+    sleep 3600
+    docker exec -it $LC /bin/bash -c "update-ca-certificates"
+done & > ~/nohup_sleep.txt 2>&1
+
 tail -f ~/nohup_*.txt
